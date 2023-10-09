c752ceb8714d499ca0d31c36f115f376d6c99f0a
vikttur <105513387+vikttur@users.noreply.github.com>
2023-10-09 02:02:31 +0000

    Deploying to gh-pages from @ vikttur/car-rental@7d717a1250b3a2c2e47f9c9ef519a33bbbf3c0fe 🚀

--- static/js/395.049137f5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcar_rental=self.webpackChunkcar_rental||[]).push([[395],{395:function(e,r,n){n.r(r);var a=n(184);r.default=function(){return(0,a.jsx)("div",{children:(0,a.jsx)("h1",{children:"favorites page"})})}}}]);
-//# sourceMappingURL=395.049137f5.chunk.js.map
